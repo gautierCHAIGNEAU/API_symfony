3116318389073e8ee0a6b2c7b390d71972634ea5
Gautier <gautier.chaigneau@gmail.com>
2017-12-13 17:20:45 +0100

    ajout du .sql a la racine du projet

---
+++ api_symfony.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 13 Décembre 2017 à 16:02
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `api_symfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `client`
+--
+
+INSERT INTO `client` (`id`, `nom`, `prenom`, `adresse`) VALUES
+(1, 'Chaigneau', 'Didier', '8 rue dastes 65000 Tarbes'),
+(3, 'Lasarte', 'Marco', '25 avenue du chêne'),
+(4, 'Duclos', 'Sylvain', '3 rue des lilas');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `facture`
+--
+
+CREATE TABLE `facture` (
+  `id` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `facture`
+--
+
+INSERT INTO `facture` (`id`, `prix`, `client_id`) VALUES
+(1, 600, 1),
+(3, 550, 3),
+(4, 200, 3),
+(5, 85, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+CREATE TABLE `migration_versions` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `migration_versions`
+--
+
+INSERT INTO `migration_versions` (`version`) VALUES
+('20171213141924'),
+('20171213142224'),
+('20171213142321'),
+('20171213143547'),
+('20171213144002');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rdv`
+--
+
+CREATE TABLE `rdv` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `rdv`
+--
+
+INSERT INTO `rdv` (`id`, `date`, `heure`, `client_id`) VALUES
+(2, '2018-02-20', 14, 3),
+(3, '2018-04-10', 14, 4);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_FE86641019EB6921` (`client_id`);
+
+--
+-- Index pour la table `migration_versions`
+--
+ALTER TABLE `migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `rdv`
+--
+ALTER TABLE `rdv`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_10C31F8619EB6921` (`client_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `rdv`
+--
+ALTER TABLE `rdv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD CONSTRAINT `FK_FE86641019EB6921` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+
+--
+-- Contraintes pour la table `rdv`
+--
+ALTER TABLE `rdv`
+  ADD CONSTRAINT `FK_10C31F8619EB6921` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
